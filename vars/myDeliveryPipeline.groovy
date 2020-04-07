770f0554ab9f5079362eb16b4cbf8b006dd89f8e
rajcdlmec35 <62494029+rajcdlmec35@users.noreply.github.com>
2020-04-08 02:13:48 +0530

    Create myDeliveryPipeline.groovy

---
+++ vars/myDeliveryPipeline.groovy
@@ -0,0 +1,17 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    sh 'echo "shared pipeline"'
+                }
+            }
+    }
+}
